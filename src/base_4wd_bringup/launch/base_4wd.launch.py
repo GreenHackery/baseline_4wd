a711dcc0afa2d51ca03ffcf758bfadeb34daf4c4
daniel <foodlikeair@icloud.com>
2026-05-13 16:43:36 -0700

    made some more updates to get it working

--- src/base_4wd_bringup/launch/base_4wd.launch.py
+++ src/base_4wd_bringup/launch/base_4wd.launch.py
@@ -6,13 +6,13 @@ from launch_ros.actions import Node
 import os
 
 def generate_launch_description():
-    robot_description_path = get_package_share_path('my_robot_description')
-    robot_bringup_path = get_package_share_path('my_robot_bringup')
+    robot_description_path = get_package_share_path('base_4wd_description')
+    robot_bringup_path = get_package_share_path('base_4wd_bringup')
     
-    urdf_path = os.path.join(robot_description_path, 'urdf', 'my_robot.urdf.xacro')
+    urdf_path = os.path.join(robot_description_path, 'urdf', 'base_4wd.urdf.xacro')
     rviz_config_path = os.path.join(robot_description_path, 'rviz', 'urdf_config.rviz')
     robot_description = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
-    robot_controllers = os.path.join(robot_bringup_path, 'config', 'my_robot_controllers.yaml')
+    robot_controllers = os.path.join(robot_bringup_path, 'config', 'base_4wd_controllers.yaml')
 
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
